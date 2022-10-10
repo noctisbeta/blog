9a836fe127e21de0ecb873a1b3c36df8af3d1268
Janez Emeršič <janez.emersic@tovarnaidej.si>
2022-10-10 10:51:13 +0200

    button fixes

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function BackButton() {
     return (
         <Link href="/">
-            <h1 className="absolute left-3 sm:left-9 p-3 rounded-xl hover:bg-myblue5 text-myblue1">
+            <h1 className="absolute left-3 sm:left-9 p-3 rounded-xl hover:bg-myblue5 text-myblue1 hover:cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
                 </svg>
--- src/components/PhotoArticleCard.tsx
+++ src/components/PhotoArticleCard.tsx
@@ -18,9 +18,11 @@ export default function PhotoArticleCard(article: Article, key: number | string)
                 {/* Divider row with link to full article */}
                 <div className="mt-auto flex flex-row justify-center items-center">
                     <div className="h-px w-full bg-myblue1"></div>
-                    <div className="px-4 py-2 rounded-full text-myblue1 border-myblue1 border bg-myblue4 hover:bg-myblue5">
-                        <Link href={`/articles/${article.slug}`}>VEČ</Link>
-                    </div>
+                    <Link href={`/articles/${article.slug}`}>
+                        <div className="px-4 py-2 rounded-full text-myblue1 border-myblue1 border bg-myblue4 hover:bg-myblue5 hover:cursor-pointer">
+                            VEČ
+                        </div>
+                    </Link>
                     <div className="h-px w-full bg-myblue1"></div>
                 </div>
 
