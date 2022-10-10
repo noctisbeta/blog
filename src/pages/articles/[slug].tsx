e433c62bd0cd24d2f07c69b49077571a9aa9ef12
Janez Emeršič <janez.emersic@tovarnaidej.si>
2022-10-10 18:02:43 +0200

    width fix

--- src/pages/articles/[slug].tsx
+++ src/pages/articles/[slug].tsx
@@ -35,29 +35,31 @@ export default function ArticleDetailPage({ article }: { article: any }) {
 
       {/* Gap */}
       <div className="h-3"></div>
-
-      <div className="flex flex-col items-center">
-        {/* Content */}
-        <div className="flex justify-center items-center">
-          <div className="text-myblue1 max-w-prose">
+      <div className="flex flex-row">
+        <div className="flex-1"></div>
+        <div className="w-[min(65ch,100%)] justify-center items-center">
+          {/* Content */}
+          <div className="flex flex-row justify-center items-center">
             <div
-              className="text-myblue1"
+              className="text-myblue1 max-w-prose"
               dangerouslySetInnerHTML={{ __html: article.content.html }}
             ></div>
+            <div className="flex-1"></div>
           </div>
-        </div>
 
-        <div className="rounded-lg overflow-hidden mt-6 max-w-prose w-full">
-          <Image
-            src={article.image.url}
-            layout="responsive"
-            width={458}
-            height={280}
-            objectFit="cover"
-          />
-        </div>
+          <div className="rounded-lg overflow-hidden mt-6 max-w-prose w-full">
+            <Image
+              src={article.image.url}
+              layout="responsive"
+              width={458}
+              height={280}
+              objectFit="cover"
+            />
+          </div>
 
-        <div className="h-12"></div>
+          <div className="h-12"></div>
+        </div>
+        <div className="flex-1"></div>
       </div>
     </div>
   );
