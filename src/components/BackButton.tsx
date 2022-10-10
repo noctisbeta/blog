efd01babccd179092c1c5e2bea064cead0833ed9
Janez Emeršič <janez.emersic@tovarnaidej.si>
2022-10-10 10:46:21 +0200

    hygraph api refactor

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function BackButton() {
     return (
-        <h1 className="absolute left-3 sm:left-9 p-3 rounded-xl hover:bg-myblue5 text-myblue1">
-            <Link href="/">
+        <Link href="/">
+            <h1 className="absolute left-3 sm:left-9 p-3 rounded-xl hover:bg-myblue5 text-myblue1">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
                 </svg>
-            </Link>
-        </h1>
+            </h1>
+        </Link>
     )
 }
