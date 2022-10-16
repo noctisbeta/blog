17df0b9315cb4bd73beec59a7c812a39682631d2
Janez Emeršič <janez.emersic@tovarnaidej.si>
2022-10-16 22:10:29 +0200

    seo wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,13 @@ export default function Home({ articles }: { articles: any }) {
   return (
     <>
       <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <title>Pišem</title>
+        <meta name="description" content="Pišem" />
+        <meta name="keywords" content="Pišem" />
+        <meta name="author" content="Robi Emeršič" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
